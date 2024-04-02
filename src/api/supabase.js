a9cb23fda4dd4e34d8481009adf421e89e0cee7a
MontseRPA <montse.pala@hotmail.com>
2024-04-02 19:42:31 +0200

    supabase keys

--- src/api/supabase.js
+++ src/api/supabase.js
@@ -1,8 +1,16 @@
 const API_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwenJhc2xod2R2ZG9pa2FrdWZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA4NzkwMzMsImV4cCI6MjAyNjQ1NTAzM30.nqfuLRg_URrz3x0fMBw3CPj1YV9QCdedHkNief23aU4'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhqem54eGZkdXZ3aWhjamp3YnpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIwNzg1MDQsImV4cCI6MjAyNzY1NDUwNH0.O8bnsmrzXp2VuMJ1L2aRzt_1cT1R0trQvJ5NEIGkWXg'
 
-const PROJECT_URL = 'https://wpzraslhwdvdoikakufh.supabase.co'
+const PROJECT_URL = 'https://hjznxxfduvwihcjjwbzs.supabase.co'
 
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(PROJECT_URL, API_KEY)
+
+
+
+// import { createClient } from '@supabase/supabase-js'
+
+// const supabaseUrl = 'https://hjznxxfduvwihcjjwbzs.supabase.co'
+// const supabaseKey = process.env.SUPABASE_KEY
+// const supabase = createClient(supabaseUrl, supabaseKey)
